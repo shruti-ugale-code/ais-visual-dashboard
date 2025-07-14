752625efb777940fc2b496a532761e8ae38b2807
shruti <122924152+shruti-ugale-code@users.noreply.github.com>
2025-07-14 14:35:21 +0530

    visuals.py

--- visuals.py
+++ visuals.py
@@ -3,29 +3,28 @@ import folium
 from streamlit_folium import st_folium
 import streamlit as st
 
-# Load data
+
 df = pd.read_csv("ship_data_sample.csv")
 
-# Update columns (17 columns as per your CSV)
-# This is correct for your file
+
 df.columns = ['MMSI', 'Date', 'Latitude', 'Longitude', 'Speed', 'Course', 'ShipType',
               'VesselName', 'IMO', 'CallSign', 'Length', 'Width', 'Draft',
               'Cargo', 'HazardousCargo', 'Region']
 
 
-# Drop rows with missing coordinates
+
 df = df.dropna(subset=["Latitude", "Longitude"])
 
-# Group by ShipName
+
 grouped = df.groupby("VesselName")
 
-# Create Streamlit app
+
 st.title("Ship Trajectories on Map")
 
-# Create base map
+
 m = folium.Map(location=[df["Latitude"].mean(), df["Longitude"].mean()], zoom_start=5)
 
-# Plot each ship's trajectory
+
 for name, group in grouped:
     points = list(zip(group["Latitude"], group["Longitude"]))
     folium.PolyLine(points, color="blue", weight=2.5, opacity=1).add_to(m)
@@ -40,5 +39,5 @@ for name, group in grouped:
             popup=f"{name}\n{row['Date']}",
         ).add_to(m)
 
-# Show the map in Streamlit
+
 st_folium(m, width=1200, height=700)
